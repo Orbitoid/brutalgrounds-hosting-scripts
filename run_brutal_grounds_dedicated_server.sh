9857f56d7e6fc2dd0bad5e3f89fe1c05fe42e1ec
Orbitoid <Orbitoid@users.noreply.github.com>
2022-01-02 02:16:53 +1100

    init commit

---
+++ run_brutal_grounds_dedicated_server.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# This setup has been running fine for almost a year with regular updates via crontab job
+# I'm using a t2.micro EC2 instance on AWS in the Sydney region and it seems to be able to handle at least 10 people at once. 
+
+# STEAM ID to use as admin in server.
+# Use the steamID64 (Dec) value: https://steamidfinder.com/
+
+ADMIN_RCON_STEAM_ID="12345678901234567"
+ADMIN_USERNAME="Your_Steam_Username"
+ADMIN_USER_PASSWORD="yourSteamUserNamesPassword"
+CURRENT_TIME=$(date +%F_%H-%M-%S)
+CURRENT_USER_ID=$(id -u)
+CURRENT_USER_GROUP_ID=$(id -g)
+BRUTAL_GROUNDS_APP_ID="1123110"
+SERVER_NAME="A descriptive name that players will see"
+HOST_NAME="Who is hosting this server?"
+HOSTING_PORT="7777"
+SEARCH_QUERY_PORT="27015"
+BRUTAL_GROUNDS_USER_DIR="/home/ubuntu/docker_home_dir"
+SSL_CERT_DIR="/etc/ssl/certs/"
+STEAM_DATA_DIR="/home/ubuntu/data/"
+
+#Guide:
+#https://github.com/AGOG-Entertainment/brutal-grounds-dedicated-server/
+
+#Update and validate the server:
+docker run \
+        --rm \
+        -v /home/ubuntu/data:/data \
+        steamcmd/steamcmd:latest +login "${ADMIN_USERNAME}" "${ADMIN_USER_PASSWORD}" +force_install_dir /data/brutal_grounds_ds +app_update "${BRUTAL_GROUNDS_APP_ID}" validate +quit
+
+docker run \
+        -v /home/ubuntu/data/brutal_grounds_ds:/data \
+        --rm \
+        ubuntu chown -R "${CURRENT_USER_ID}:${CURRENT_USER_GROUP_ID}" /data
+
+# run the server
+docker run \
+--rm \
+-v "${STEAM_DATA_DIR}":/data \
+-v "${SSL_CERT_DIR}":/etc/ssl/certs/ \
+-v "${BRUTAL_GROUNDS_USER_DIR}":/home/bg_user \
+-u ${CURRENT_USER_ID}:${CURRENT_USER_GROUP_ID} \
+-p 7777:7777/udp \
+-p 27015:27015/udp \
+-e HOME=/home/bg_user \
+ubuntu /data/brutal_grounds_ds/BrutalGroundsServer.sh \
+Arena \
+-log \
+-nullrhi \
+-AdminId="${ADMIN_RCON_STEAM_ID}" \
+-SteamServerName="${SERVER_NAME}" \
+-HostName="${HOST_NAME}" \
+-Port="${HOSTING_PORT}" \
+-QueryPort="${SEARCH_QUERY_PORT}"
